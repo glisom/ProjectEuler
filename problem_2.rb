87ff24d9b0db5f1ebb30f72f9cb30a743c38baf1
glisom <grantisom@ku.edu>
2014-08-15 09:59:58 -0500

    soltuions

--- problem_2.rb
+++ problem_2.rb
@@ -2,9 +2,7 @@ a = [1,1]
 while a[-2] + a[-1] < 4000000
   a << a[-2] + a[-1]
 end
-
-# Create sum of even Fibonnaci numbers
 sum = 0
 a.each { |x| sum+= x if x.even? }
 
-puts "The result is #{sum}"
+puts sum
